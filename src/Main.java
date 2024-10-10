7cc5454d546cfb6f55029bd14ccee81d47efc6ad
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-10 19:31:44 +0200

    nine commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.printf("Hello and welcome!7");
         System.out.printf("Hello and welcome!8");
         System.out.printf("Hello and welcome!9");
+        System.out.printf("Hello and welcome!10");
+
 
     }
 }
