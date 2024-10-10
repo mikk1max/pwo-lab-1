c1935ab2089e8566142afbc42beb7a582cfdaaca
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-10 19:00:52 +0200

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!2");
         System.out.printf("Hello and welcome!3");
+        System.out.printf("Hello and welcome!4");
     }
 }
